73674e4b2dc3ea5c1e4566668a6971e496307c0f
Kranc77 <andrzejkrawiec77@o2.pl>
2021-01-02 18:05:39 +0100

    sortowania i funkcja czasu

--- projekt_2.py
+++ projekt_2.py
@@ -1,4 +1,5 @@
 import os
+import time
 import random # biblioteka dzięki której będziemy renderować losowe liczby
 def sortowanie_gnom(list):
     pozycja = 0
@@ -8,33 +9,29 @@ def sortowanie_gnom(list):
         else:
             list[pozycja - 1],list[pozycja] = list[pozycja], list[pozycja-1] # w c++ musielibyśmy użyć tu funkcji "swap" albo wprowadzić nową zmienną pomocniczą o ile się nie mylę
             # a w pythonie jest takie właśnie proste obejście związane z zamianą pozycji,
-            pozycja-=1 # gdy lista nie była posortowana i musieliśmy zamienić liczby w liście to musimy zmniejszyć pozycję by sprawdzić czy zamieniona liczba jest w na odpowiednim miejscu
+            pozycja-=1 # gdy lista nie była posortowana i musieliśmy zamienić liczby w liście to musimy zmniejszyć pozycję
+            # by sprawdzić czy zamieniona liczba jest w na odpowiednim miejscu
 
 def sortowanie_kubek(list):
-    a = min(list)
+    a = min(list) # wykorzystałem funkcję min i max dostępne w pyhtonie
     b = max(list)
-    kubki = [0]* ((b-a)+1)
-    print(a)
-    print(b)
+    kubki = [0]* ((b-a)+1) # wzór znaleziony w internecie który na podstawie zróżnicowania liczb określa ile kubeczków stworzymy
     for i in range(0,((b-a))):
         kubki[i]=list.count(i)
     print(kubki)
-    list2 = []
-    for i in range(0, ((b - a) + 1)):
-        for x in range(0,kubki[i]):
-            list2.append(i)
-    print(list2)
-
-
-
-
-''' for x in f:
-    x = x.replace("\n", "")
-    list1.append(int(x)) #dodawanie do listy elementów które są oddzielane znakiem /n czyli enterem(są w nowej lini) '''
+    licznik = 0 # zmienna mówiąca nam o indeksach listy
+    for i in range(0, ((b - a) + 1)): # sprawdzamy podany przedział
+        for x in range(0,kubki[i]): # pętla zamieniające ineksy listy INDEKSAMI kubeczków w zależności od WARTOŚCI kubeczka
+            list[licznik]=i
+            licznik+=1
 list1 =[]
-for i in range(0,100):
+for i in range(0,10000):
     list1.append(random.randint(0,100))
 print(list1)
-#sortowanie_gnom(list1)
-#print(list1)
-sortowanie_kubek(list1)
+start = time.time()
+sortowanie_gnom(list1)
+#sortowanie_kubek(list1)
+print(list1)
+end = time.time()
+total = end - start
+print(total)
