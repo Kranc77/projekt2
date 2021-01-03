b963228dfa1a2e970d19d0da0d06df336f4f980f
Kranc77 <andrzejkrawiec77@o2.pl>
2021-01-03 19:04:50 +0100

    działa poprawnie - 19:04 - 03.01

--- projekt_2.py
+++ projekt_2.py
@@ -15,30 +15,18 @@ def sortowanie_gnom(list):
 def sortowanie_kubek(list):
     a = min(list) # wykorzystałem funkcję min i max dostępne w pyhtonie
     b = max(list)
-    kubki = [0]* (len(list)+1)
-    for i in range(a,b+1):
-        kubki[i]=list.count(i)
+    kubki = [0]* ((b-a)+1) # tworzę możliwie najmnije kubełków
+    for i in range(0,((b-a)+1)):
+        kubki[i] = list.count(a+i) # przypisanie do pierwszego kubełka ilości najmniejszych wartości
     print(kubki)
     licznik = 0 # zmienna mówiąca nam o indeksach listy
-    for i in range(a,b+1): # sprawdzamy podany przedział
+    for i in range(0,((b-a)+1)): # sprawdzamy podany przedział
         for x in range(0,kubki[i]): # pętla zamieniające ineksy listy INDEKSAMI kubeczków w zależności od WARTOŚCI kubeczka
-            list[licznik]=i
-            licznik+=1
-def sortowanie_kubek_v2(list,n):
-    a = min(list)
-    b = max(list)
-    kubki = [n-1]
-    for i in range(0,((b-a))):
-        kubki[i]=list.count(i)
-    print(kubki)
-    licznik = 0
-    for i in range(0, ((b - a) + 1)):
-        for x in range(0,kubki[i]):
-            list[licznik]=i
+            list[licznik]=a+i
             licznik+=1
 list1 =[]
-for i in range(0,20):
-    list1.append(random.randint(0,10))
+for i in range(0,1000): # ilość danych
+    list1.append(random.randint(0,100000))# zróżnicowanie danych
 print(list1)
 start = time.time()
 #sortowanie_gnom(list1)
